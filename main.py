a2c7d4a43e5a474ecdc76ef95106feff4471365e
Matthew LeBlanc <matthewmleblanc@gmail.com>
2025-03-30 13:55:55 -0500

    added game clock

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     
     while 1:
         for event in pygame.event.get():
@@ -14,7 +16,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-    
+        dt = game_clock.tick(60) / 1000 
 
 
 if __name__ == '__main__':
